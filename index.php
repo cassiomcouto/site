644822705e662a46176bb9d662e64b3b9946632c
Cássio Couto <cassiomcouto@gmail.com>
2015-06-06 22:24:51 -0300

    Apresentacao

--- index.php
+++ index.php
@@ -1,71 +1,92 @@
-<?php
-/**
- * start page for webaccess
- * redirect the user to the supported page type by the users webbrowser (js available or not)
- *
- * PHP version 5
- *
- * @category  PHP
- * @package   PSI
- * @author    Michael Cramer <BigMichi1@users.sourceforge.net>
- * @copyright 2009 phpSysInfo
- * @license   http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @version   SVN: $Id: index.php 687 2012-09-06 20:54:49Z namiltd $
- * @link      http://phpsysinfo.sourceforge.net
- */
-/**
- * define the application root path on the webserver
- * @var string
- */
-define('APP_ROOT', dirname(__FILE__));
 
-/**
- * internal xml or external
- * external is needed when running in static mode
- *
- * @var boolean
- */
-define('PSI_INTERNAL_XML', false);
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
+             <!--
+    Important Note About This Website's SEO
 
-if (version_compare("5.2", PHP_VERSION, ">")) {
-    die("PHP 5.2 or greater is required!!!");
-}
-if (!extension_loaded("pcre")) {
-    die("phpSysInfo requires the pcre extension to php in order to work properly.");
-}
+    Find the SEO content of this site's homepage via http://cassiomcouto.wix.com/cassio?_escaped_fragment_=
+    (That is where search engines like Google go to read your homepage's content.)
 
-require_once APP_ROOT.'/includes/autoloader.inc.php';
+    To view the SEO content of your internal pages, such as "CONTATO", go here: http://cassiomcouto.wix.com/cassio?_escaped_fragment_=contact/c1d94
+    (That is where search engines like Google go to read the content on your internal pages.)
 
-// Load configuration
-require_once APP_ROOT.'/read_config.php';
+    For more information about Ajax Crawling technology, read Google's explanation here: https://developers.google.com/webmasters/ajax-crawling/
+-->
 
-if (!defined('PSI_CONFIG_FILE') || !defined('PSI_DEBUG')) {
-    $tpl = new Template("/templates/html/error_config.html");
-    echo $tpl->fetch();
-    die();
-}
+    
+                <meta charset="utf-8"/>
+<title>Cássio Couto</title>
+<meta name="fb_admins_meta_tag" content=""/>
+<link rel="shortcut icon" href="http://www.wix.com/favicon.ico" type="image/x-icon"/>
+<link rel="apple-touch-icon" href="http://www.wix.com/favicon.ico" type="image/x-icon"/>
+<link rel="canonical" href="http://cassiomcouto.wix.com/cassio"/>
+    <meta http-equiv="X-Wix-Renderer-Server" content="app29.aus"/>
+<meta http-equiv="X-Wix-Meta-Site-Id" content="ead00c4a-57af-47cf-b3dd-0560c9372493"/>
+<meta http-equiv="X-Wix-Application-Instance-Id" content="34164a86-32b7-4ce9-8883-8b5e34ead4ba"/>
+<meta http-equiv="X-Wix-Published-Version" content="2"/>
 
-// redirect to page with and without javascript
-$display = isset($_GET['disp']) ? $_GET['disp'] : strtolower(PSI_DEFAULT_DISPLAY_MODE);
-switch ($display) {
-case "static":
-    $webpage = new WebpageXSLT();
-    $webpage->run();
-    break;
-case "dynamic":
-    $webpage = new Webpage();
-    $webpage->run();
-    break;
-case "xml":
-    $webpage = new WebpageXML(true, null);
-    $webpage->run();
-    break;
-case "bootstrap":
-    $tpl = new Template("/templates/html/index_bootstrap.html");
-    echo $tpl->fetch();
-    break;
-default:
-    $tpl = new Template("/templates/html/index_all.html");
-    echo $tpl->fetch();
-    break;
-}
+<meta http-equiv="etag" content="9ee1d0ec91b35026e04f21858bf9ea0c"/>
+<meta property="og:title" content="cassio"/>
+<meta property="og:type" content="article"/>
+<meta property="og:url" content="http://cassiomcouto.wix.com/cassio"/>
+<meta property="og:site_name" content="cassio"/>
+<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE"/>
+
+<meta id="wixMobileViewport" name="viewport" content="minimum-scale=0.25, maximum-scale=1.2"/>
+
+
+        
+
+            <script>
+            // BEAT MESSAGE
+    try {
+        window.wixBiSession = {
+            initialTimestamp : Date.now(),
+            viewerSessionId: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+                    { var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8); return v.toString(16); }
+            )
+        };
+        (new Image()).src = 'http://frog.wix.com/bt?src=29&evid=3&pn=1&et=1&v=3.0&vsi=' + wixBiSession.viewerSessionId +
+                '&url=' + encodeURIComponent(location.href.replace(/^http(s)?:\/\/(www\.)?/, '')) +
+                '&isp=0&st=2&ts=0&c=' + wixBiSession.initialTimestamp;
+    } catch (e){}
+    // BEAT MESSAGE END
+</script>    
+    
+
+    <!-- META DATA -->
+<script type="text/javascript">
+
+    var serviceTopology = {"serverName":"app29.aus","cacheKillerVersion":"1","staticServerUrl":"http://static.parastorage.com/","usersScriptsRoot":"http://static.parastorage.com/services/wix-users/2.487.0","biServerUrl":"http://frog.wix.com/","userServerUrl":"http://users.wix.com/","billingServerUrl":"http://premium.wix.com/","mediaRootUrl":"http://static.wixstatic.com/","logServerUrl":"http://frog.wix.com/plebs","monitoringServerUrl":"http://TODO/","usersClientApiUrl":"https://users.wix.com/wix-users","publicStaticBaseUri":"http://static.parastorage.com/services/wix-public/1.160.0","basePublicUrl":"http://www.wix.com/","postLoginUrl":"http://www.wix.com/my-account","postSignUpUrl":"http://www.wix.com/new/account","baseDomain":"wix.com","staticMediaUrl":"http://static.wixstatic.com/media","staticAudioUrl":"http://storage.googleapis.com/static.wixstatic.com/mp3","emailServer":"http://assets.wix.com/common-services/notification/invoke","blobUrl":"http://static.parastorage.com/wix_blob","htmlEditorUrl":"http://editor.wix.com/html","siteMembersUrl":"https://users.wix.com/wix-sm","scriptsLocationMap":{"wixapps":"http://static.parastorage.com/services/wixapps/2.461.12","tpa":"http://static.parastorage.com/services/tpa/2.1041.0","santa-resources":"http://static.parastorage.com/services/santa-resources/1.0.0","bootstrap":"http://static.parastorage.com/services/bootstrap/2.1144.74","ck-editor":"http://static.parastorage.com/services/ck-editor/1.87.2","it":"http://static.parastorage.com/services/experiments/it/1.37.0","skins":"http://static.parastorage.com/services/skins/2.1144.74","core":"http://static.parastorage.com/services/core/2.1144.74","sitemembers":"http://static.parastorage.com/services/sm-js-sdk/1.31.0","automation":"http://static.parastorage.com/services/automation/1.23.0","web":"http://static.parastorage.com/services/web/2.1144.74","ecommerce":"http://static.parastorage.com/services/ecommerce/1.190.0","hotfixes":"http://static.parastorage.com/services/experiments/hotfixes/1.15.0","langs":"http://static.parastorage.com/services/langs/2.553.0","santa-versions":"http://static.parastorage.com/services/santa-versions/1.279.0","ut":"http://static.parastorage.com/services/experiments/ut/1.2.0"},"developerMode":false,"productionMode":true,"staticServerFallbackUrl":"https://sslstatic.wix.com/","staticVideoUrl":"http://storage.googleapis.com/video.wixstatic.com","userFilesUrl":"http://static.parastorage.com/","staticHTMLComponentUrl":"http://cassiomcouto.wix.com.usrfiles.com/","secured":false,"ecommerceCheckoutUrl":"https://www.safer-checkout.com/","premiumServerUrl":"https://premium.wix.com/","appRepoUrl":"http://assets.wix.com/wix-lists-ds-webapp","digitalGoodsServerUrl":"http://dgs.wixapps.net/","wixCloudBaseDomain":"cloud.wix.com","publicStaticsUrl":"http://static.parastorage.com/services/wix-public/1.160.0","staticDocsUrl":"http://media.wix.com/ugd"};
+        var santaModels = true;
+    var rendererModel = {"metaSiteId":"ead00c4a-57af-47cf-b3dd-0560c9372493","siteInfo":{"documentType":"UGC","applicationType":"HtmlWeb","siteId":"34164a86-32b7-4ce9-8883-8b5e34ead4ba","siteTitleSEO":"cassio"},"clientSpecMap":{"0":{"type":"appbuilder","applicationId":0,"appDefinitionId":"3d590cbc-4907-4cc4-b0b1-ddf2c5edf297","instanceId":"13e54fb0-ff4f-a037-b596-115275191f4e","state":"Initialized"},"11":{"type":"sitemembers","applicationId":11,"collectionType":"Open","smcollectionId":"69c35c49-5ddb-408f-9466-5912e8ab44b7","smtoken":"0693f1df9b384552828e9a0f443d98b33fc50fb1f474d8de9cd1ab2236c6e3c5b2b9829a2709165fdcb997ab84a8f8c3dcea40706c8e712c6e67b359d1f1b66e48e3f0580d7ff67a7379a3ad0444417c2cda977f88b7b70a7989175a73c63e54"}},"premiumFeatures":[],"geo":"BRA","languageCode":"pt","previewMode":false,"userId":"9205bdce-46c6-4fa8-856e-b320cfa10561","siteMetaData":{"preloader":{"enabled":false},"adaptiveMobileOn":true,"quickActions":{"socialLinks":[],"colorScheme":"dark","configuration":{"quickActionsMenuEnabled":false,"navigationMenuEnabled":true,"phoneEnabled":false,"emailEnabled":false,"addressEnabled":false,"socialLinksEnabled":false}},"contactInfo":{"companyName":"","phone":"","fax":"","email":"","address":""}},"runningExperiments":{"autogeneratedshapesskins":"new","lang_no":"new","ngcore":"new","sandboxiframeineditor":"new","lang_nl":"new","wixappsgalleries":"new","ecomgalleries":"new","disablehorizontalmenu":"new","lazyprovision":"new","atntfixlists":"new","exitmobilemode":"new","workaroundsaveddeadcompskin":"new","nougcanalytics":"new","appbuilderdeletetype":"new","sitepagesvalidation":"new","redirectfeature301data":"new","linkfixeroverride":"new","redirectfeature301":"new","lazyprovisiontemp":"new","animation3dfix":"new","landingpagesupport":"new","lang_sv":"new","wixappstranslation":"new","subscribeformsendnewsletter":"new","lesserwidthissue":"new","appbuildertags":"new","sitenavigationrefactor":"new","blogrss":"new","animationnewbehaviors":"new","customsitemenu":"new","blogmanager":"new"}};
+    var publicModel = {"domain":"wix.com","externalBaseUrl":"http:\/\/cassiomcouto.wix.com\/cassio","unicodeExternalBaseUrl":"http:\/\/cassiomcouto.wix.com\/cassio","pageList":{"masterPage":["http:\/\/static.wixstatic.com\/sites\/9205bd_129aef92b686c10850fd76568b454d0f_1.json.z?v=3","http:\/\/staticorigin.wixstatic.com\/sites\/9205bd_129aef92b686c10850fd76568b454d0f_1.json.z?v=3","http:\/\/fallback.wix.com\/wix-html-editor-pages-webapp\/page\/9205bd_129aef92b686c10850fd76568b454d0f_1.json"],"pages":[{"pageId":"c1d94","title":"CONTATO","urls":["http:\/\/static.wixstatic.com\/sites\/9205bd_52bfb65393acfc8a6668c40b1576facc_1.json.z?v=3","http:\/\/staticorigin.wixstatic.com\/sites\/9205bd_52bfb65393acfc8a6668c40b1576facc_1.json.z?v=3","http:\/\/fallback.wix.com\/wix-html-editor-pages-webapp\/page\/9205bd_52bfb65393acfc8a6668c40b1576facc_1.json"]},{"pageId":"cm8a","title":"PROJETOS","urls":["http:\/\/static.wixstatic.com\/sites\/9205bd_f0e575517b06e4802d64707d20c98f33_1.json.z?v=3","http:\/\/staticorigin.wixstatic.com\/sites\/9205bd_f0e575517b06e4802d64707d20c98f33_1.json.z?v=3","http:\/\/fallback.wix.com\/wix-html-editor-pages-webapp\/page\/9205bd_f0e575517b06e4802d64707d20c98f33_1.json"]},{"pageId":"c46c","title":"CURRÍCULO","urls":["http:\/\/static.wixstatic.com\/sites\/9205bd_4cc8656e5b4415a7ca99b312ab395d20_1.json.z?v=3","http:\/\/staticorigin.wixstatic.com\/sites\/9205bd_4cc8656e5b4415a7ca99b312ab395d20_1.json.z?v=3","http:\/\/fallback.wix.com\/wix-html-editor-pages-webapp\/page\/9205bd_4cc8656e5b4415a7ca99b312ab395d20_1.json"]},{"pageId":"mainPage","title":"HOME","urls":["http:\/\/static.wixstatic.com\/sites\/9205bd_4b84320d8fa0940094752e9efb654776_1.json.z?v=3","http:\/\/staticorigin.wixstatic.com\/sites\/9205bd_4b84320d8fa0940094752e9efb654776_1.json.z?v=3","http:\/\/fallback.wix.com\/wix-html-editor-pages-webapp\/page\/9205bd_4b84320d8fa0940094752e9efb654776_1.json"]}],"mainPageId":"mainPage"},"timeSincePublish":12246,"favicon":""};
+    
+
+
+    var googleAnalytics = ""
+    ;
+
+    var googleRemarketing = "";
+    var facebookRemarketing = "";
+
+</script>
+
+
+
+    </head>
+<body>
+                <div id="SITE_CONTAINER"></div>
+    
+    
+        
+
+    
+    
+    
+        
+    
+</body>
+</html>
